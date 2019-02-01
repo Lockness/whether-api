ee824fb99050128127c36c4f6eb6b89c06b95e00
Ryan Tomlinson <ryantomlinson95@gmail.com>
2019-01-31 21:53:09 -0500

    Hotfix/cleanup (#9)
    * update algo and associated static methods; speed improvements
    
    * convert haversine to miles
    
    * convert get_equi to miles
    
    * conv move_towards to miles
    
    * removing commented out funcs

--- src/modules/WhetherAlgorithm.py
+++ src/modules/WhetherAlgorithm.py
@@ -143,34 +143,6 @@ class WhetherAlgorithm:
 
         return np.array([math.degrees(lat2), math.degrees(lon2)])
 
-    # def iterative_move_along_path(self, points, distance, index=0):
-    #     while index < len(points) - 1:
-    #         # There is still at least one point further from this point
-    #         # Turn points into tuples for geopy format
-    #         # point1_tuple = (points[index]['latitude'], points[index]['longitude'])
-    #         # point2_tuple = (points[index + 1]['latitude'], points[index + 1]['longitude'])
-    #         point1_tuple = (points[index]['lat'], points[index]['lng'])
-    #         point2_tuple = (points[index + 1]['lat'], points[index + 1]['lng'])
-    #
-    #         # Use geodesic method to get distance between points in meters
-    #         distance_to_next_point = self.haversine(point1_tuple, point2_tuple)
-    #
-    #         if distance <= distance_to_next_point:
-    #             # Distance_to_next_point is within this point and the next
-    #             # Return the destination point with moveTowards()
-    #             return self.move_towards(point1_tuple, point2_tuple, distance)
-    #
-    #         else:
-    #             # The destination is further from the next point
-    #             # Subtract distance_to_next_point from distance and continue recursively
-    #             distance -= distance_to_next_point
-    #             index += 1
-    #
-    #     # There are no further points, the distance exceeds the length of the full path.
-    #     # Return None
-    #     return None
-
-
     @staticmethod
     def haversine(coords):
         """
@@ -195,33 +167,6 @@ class WhetherAlgorithm:
         miles = 3959 * c
 
         return miles
-
-    # def move_along_path(self, points, distance, index=0):
-    #     if index < len(points) - 1:
-    #         # There is still at least one point further from this point
-    #         # Turn points into tuples for geopy format
-    #         # point1_tuple = (points[index]['latitude'], points[index]['longitude'])
-    #         # point2_tuple = (points[index + 1]['latitude'], points[index + 1]['longitude'])
-    #         point1_tuple = (points[index]['lat'], points[index]['lng'])
-    #         point2_tuple = (points[index + 1]['lat'], points[index + 1]['lng'])
-    #
-    #         # Use geodesic method to get distance between points in meters
-    #         distance_to_next_point = geopy.distance.geodesic(point1_tuple, point2_tuple).m
-    #
-    #         if distance <= distance_to_next_point:
-    #             # Distance_to_next_point is within this point and the next
-    #             # Return the destination point with moveTowards()
-    #             return self.move_towards(point1_tuple, point2_tuple, distance)
-    #
-    #         else:
-    #             # The destination is further from the next point
-    #             # Subtract distance_to_next_point from distance and continue recursively
-    #             return self.move_along_path(points, distance - distance_to_next_point, index + 1)
-    #
-    #     else:
-    #         # There are no further points, the distance exceeds the length of the full path.
-    #         # Return None
-    #         return None
 
     def get_waypoint_directions(self, params, equidistant_markers):
         # Set the origin of the directions as the first waypoint so it doesn't calculate a leg of 1 minute
@@ -255,27 +200,3 @@ class WhetherAlgorithm:
             i += 1
 
         return waypoints_result, equidistant_markers
-
-    # NOT CURRENTLY IN USE BUT MIGHT NEED IT AT SOME POINT
-    # def get_snapped_points(self, points):
-    #     PAGINATION_OVERLAP = 10
-    #     PAGE_SIZE_LIMIT = 100
-    #     all_snapped_points = []
-    #     offset = 0
-    #     while offset < len(points):
-    #         if offset > 0:
-    #             offset -= PAGINATION_OVERLAP
-    #
-    #         lowerBound = offset;
-    #         upperBound = min(offset + PAGE_SIZE_LIMIT, len(points))
-    #         page = points[lowerBound:upperBound]
-    #         page_snapped_points = self.googlemaps_client.snap_to_roads(page, interpolate=True)
-    #         passedOverlap = False
-    #         for point in page_snapped_points:
-    #             if passedOverlap or (offset == 0) or (point['originalIndex'] >= PAGINATION_OVERLAP - 1):
-    #                 passedOverlap = True
-    #                 all_snapped_points.append(point['location'])
-    #
-    #         offset = upperBound
-    #
-    #     return all_snapped_points
