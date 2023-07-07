e02ee29b745a57ed2e4ac35aa740929a97b6381a
Ryan Tomlinson <ryantomlinson95@gmail.com>
2023-07-07 18:22:08 -0500

    Update GoogleClient.py

--- src/modules/GoogleClient.py
+++ src/modules/GoogleClient.py
@@ -1,8 +1,5 @@
 import googlemaps
 
-import src.constants as c
-
-
 class GoogleClient:
     def __init__(self):
-        self.client = googlemaps.Client(key=c.api_key)
+        self.client = googlemaps.Client(key=os.environ['API_KEY'])
